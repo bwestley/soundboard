82600cc61a358bbd66b55b77dcc746471223085d
Brendan Westley <brendan@westley.us>
2023-06-05 21:24:51 -0500

    Create iced window

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use iced::widget::{button, slider, text, text_input, Column, Row};
+use iced::{executor, Alignment, Application, Command, Element, Settings, Theme};
+use rodio::buffer;
+use rodio::{source::Source, Decoder, OutputStream, OutputStreamHandle, Sink};
 use serde::{Deserialize, Serialize};
-use std::fs;
 use std::io::{prelude::*, BufReader};
+use std::net::TcpStream;
+use std::sync::mpsc;
 use std::time::SystemTime;
+use std::{fs, thread};
 mod as_hex;
 mod event;
 use event::*;
@@ -28,8 +34,8 @@ struct ShortcutsConfig {
 }
 
 /// Holds a sound configuration.
-#[derive(Serialize, Deserialize)]
-struct SoundConfig {
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct SoundConfig {
     path: String,
     name: String,
     volume: f64,
@@ -135,6 +141,23 @@ fn load_config() -> Result<Config, String> {
     }
 }
 
+/// Save the toml configuration to [`get_config_file_path`].
+fn save_config(config: &Config) -> Result<(), String> {
+    let config_file_path = get_config_file_path()?;
+    println!(
+        "[Configuration Saver] Saving configuration file \"{}\".",
+        config_file_path.display()
+    );
+
+    match toml::to_string_pretty(config) {
+        Err(error) => return Err(format!("Unable to serialize configuration file: {error}.")),
+        Ok(serialized_config) => match fs::write(&config_file_path, serialized_config) {
+            Err(error) => Err(format!("Unable to write configuration file: {error}.")),
+            Ok(_) => Ok(()),
+        },
+    }
+}
+
 /// Format a [`SystemTime`] as T+{ms} or T-{ms} relative to the current system time.
 fn format_timestamp(timestamp: SystemTime) -> String {
     match timestamp.elapsed() {
@@ -143,69 +166,258 @@ fn format_timestamp(timestamp: SystemTime) -> String {
     }
 }
 
-fn main() {
-    // Load configuration file.
-    let config = load_config().unwrap();
-    println!(
-        "[Remote Input Client] Connecting to remote input server {}.",
-        &config.server_address
-    );
+struct RemoteInputClient {
+    buffer_reader: BufReader<TcpStream>,
+    event_buffer: Vec<u8>,
+}
 
-    // Connect to the remote input server.
-    let mut stream = std::net::TcpStream::connect(&config.server_address).unwrap();
-    println!(
-        "[Remote Input Client] Connected to remote input server {}.",
-        config.server_address
-    );
+impl RemoteInputClient {
+    fn new(server_address: String, api_key: String) -> RemoteInputClient {
+        println!(
+            "[Remote Input Client] Connecting to remote input server {}.",
+            server_address
+        );
 
-    // Send the API key to the remote input server.
-    let api_key = [config.api_key.as_bytes(), &[0x00u8]].concat();
-    println!(
-        "[Remote Input Client] Sent {} byte API key.",
-        stream.write(&api_key).expect("unable to send API key")
-    );
+        // Connect to the remote input server.
+        let mut stream = std::net::TcpStream::connect(server_address.clone()).unwrap();
+        println!(
+            "[Remote Input Client] Connected to remote input server {}.",
+            server_address
+        );
 
-    // Receive events from the remote input server.
-    // Events are [`InputEventWrapper`] serialized by [`postcard`] and encoded by COBS.
-    let mut buffer_reader = BufReader::new(&mut stream);
-    let mut event_buffer = Vec::new();
-    loop {
+        // Send the API key to the remote input server.
+        let api_key = [api_key.as_bytes(), &[0x00u8]].concat();
+        println!(
+            "[Remote Input Client] Sent {} byte API key.",
+            stream.write(&api_key).expect("unable to send API key")
+        );
+
+        // Receive events from the remote input server.
+        // Events are [`InputEventWrapper`] serialized by [`postcard`] and encoded by COBS.
+        let buffer_reader = BufReader::new(stream);
+        let event_buffer = Vec::new();
+
+        RemoteInputClient {
+            buffer_reader,
+            event_buffer,
+        }
+    }
+
+    fn process_event(&mut self) -> Option<InputEventWrapper> {
         // Receive data.
-        event_buffer.clear();
-        buffer_reader
-            .read_until(0x00, &mut event_buffer)
+        self.event_buffer.clear();
+        self.buffer_reader
+            .read_until(0x00, &mut self.event_buffer)
             .expect("unable to read event");
 
         // The event should end with 0x00 because it is encoded by COBS.
-        if !event_buffer.ends_with(&[0x00]) {
+        if !self.event_buffer.ends_with(&[0x00]) {
             println!("[Remote Input Client] Connection lost.");
-            break;
+            return None;
         }
 
         // Deserialize event.
-        let event_data = event_buffer.as_mut_slice();
+        let event_data = self.event_buffer.as_mut_slice();
         println!(
             "[Remote Input Client] Received event: {}.",
             as_hex::as_hex(event_data)
         );
         match postcard::from_bytes_cobs::<InputEventWrapper>(event_data) {
             Err(deserialize_error) => {
-                println!("[Remote Input Client] Failed to deserialize event: {deserialize_error}.")
+                println!("[Remote Input Client] Failed to deserialize event: {deserialize_error}.");
+                None
             }
-            Ok(event_wrapper) => match event_wrapper.as_event() {
-                Some(enumerated_event) => {
-                    println!(
-                        "[Remote Input Client] Deserialized enumerated event: timestamp: {}, event_type: {}, code: {}, value: {}.",
-                        format_timestamp(event_wrapper.timestamp), event_wrapper.as_event_type().unwrap().as_ref(), enumerated_event.code_as_ref(), event_wrapper.value
-                    );
+            Ok(event_wrapper) => {
+                match event_wrapper.as_event() {
+                    Some(enumerated_event) => {
+                        println!(
+                            "[Remote Input Client] Deserialized enumerated event: timestamp: {}, event_type: {}, code: {}, value: {}.",
+                            format_timestamp(event_wrapper.timestamp), event_wrapper.as_event_type().unwrap().as_ref(), enumerated_event.code_as_ref(), event_wrapper.value
+                        );
+                    }
+                    None => {
+                        println!(
+                            "[Remote Input Client] Deserialized undefined event: timestamp: {}, event_type: {}, code: {}, value: {}.",
+                            format_timestamp(event_wrapper.timestamp), event_wrapper.event_type, event_wrapper.code, event_wrapper.value
+                        );
+                    }
+                };
+                Some(event_wrapper)
+            }
+        }
+    }
+}
+
+struct Soundboard {
+    config: Config,
+}
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    ConfigChanged(ConfigChanged),
+    ShortcutChanged(ShortcutChanged),
+    SoundChanged(SoundChanged),
+}
+
+#[derive(Debug, Clone)]
+pub enum ConfigChanged {
+    ServerAddress(String),
+    ApiKey(String),
+    Volume(f64),
+    AddSoundOutput(String),
+    RemoveSoundOutput(usize),
+    NotificationOutput(String),
+}
+
+#[derive(Debug, Clone)]
+pub enum ShortcutChanged {
+    Pause(u16),
+    Play(u16),
+    Disable(u16),
+    Enable(u16),
+}
+
+#[derive(Debug, Clone)]
+pub enum SoundChanged {
+    AddSound(SoundConfig),
+    RemoveSound(usize),
+    EditSound(usize, SoundConfig),
+}
+
+impl Application for Soundboard {
+    type Executor = executor::Default;
+    type Flags = ();
+    type Message = Message;
+    type Theme = Theme;
+
+    fn new(_flags: ()) -> (Self, Command<Self::Message>) {
+        (
+            Self {
+                config: load_config().unwrap(),
+            },
+            Command::none(),
+        )
+    }
+
+    fn title(&self) -> String {
+        String::from("A cool application")
+    }
+
+    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
+        match message {
+            Message::ConfigChanged(config_changed) => match config_changed {
+                ConfigChanged::ServerAddress(server_address) => {
+                    self.config.server_address = server_address
                 }
-                None => {
-                    println!(
-                        "[Remote Input Client] Deserialized undefined event: timestamp: {}, event_type: {}, code: {}, value: {}.",
-                        format_timestamp(event_wrapper.timestamp), event_wrapper.event_type, event_wrapper.code, event_wrapper.value
-                    );
+                ConfigChanged::ApiKey(api_key) => self.config.api_key = api_key,
+                ConfigChanged::Volume(volume) => self.config.volume = volume,
+                ConfigChanged::AddSoundOutput(name) => self.config.sound_outputs.push(name),
+                ConfigChanged::RemoveSoundOutput(index) => {
+                    if index <= self.config.sound_outputs.len() {
+                        self.config.sound_outputs.remove(index);
+                    }
+                }
+                ConfigChanged::NotificationOutput(name) => self.config.notification_output = name,
+            },
+            Message::ShortcutChanged(shortcut_changed) => match shortcut_changed {
+                ShortcutChanged::Pause(key_code) => self.config.shortcuts.pause = key_code,
+                ShortcutChanged::Play(key_code) => self.config.shortcuts.play = key_code,
+                ShortcutChanged::Disable(key_code) => self.config.shortcuts.disable = key_code,
+                ShortcutChanged::Enable(key_code) => self.config.shortcuts.enable = key_code,
+            },
+            Message::SoundChanged(sound_changed) => match sound_changed {
+                SoundChanged::AddSound(sound_config) => self.config.sounds.push(sound_config),
+                SoundChanged::RemoveSound(index) => {
+                    if index <= self.config.sound_outputs.len() {
+                        self.config.sounds.remove(index);
+                    }
+                }
+                SoundChanged::EditSound(index, sound_config) => {
+                    if index <= self.config.sound_outputs.len() {
+                        if index <= self.config.sound_outputs.len() {
+                            self.config.sounds.remove(index);
+                            self.config.sounds.insert(index, sound_config);
+                        }
+                    }
                 }
             },
         };
+        Command::none()
     }
+
+    fn view(&self) -> Element<Self::Message> {
+        /*Column::<Self::Message>::with_children(vec![
+            button("+").on_press(Self::Message::Increment),
+            text(self.value).size(50),
+            button("-").on_press(Self::Message::Decrement),
+        ])
+        .into()*/
+        Row::new()
+            .push(
+                Column::new()
+                    .push(text("Server Address").size(30))
+                    .push(text("API Key").size(30))
+                    .push(text("Volume").size(30))
+                    .spacing(10),
+            )
+            .push(
+                Column::new()
+                    .push(
+                        text_input("", &self.config.server_address).on_input(|input| {
+                            Self::Message::ConfigChanged(ConfigChanged::ServerAddress(input))
+                        }),
+                    )
+                    .push(text_input("", &self.config.api_key).on_input(|input| {
+                        Self::Message::ConfigChanged(ConfigChanged::ApiKey(input))
+                    }))
+                    .push(slider(
+                        0..=100,
+                        (self.config.volume * 100.0).clamp(0.0, 100.0) as i32,
+                        |volume| {
+                            Self::Message::ConfigChanged(ConfigChanged::Volume(
+                                volume as f64 / 100f64,
+                            ))
+                        },
+                    ))
+                    .spacing(10),
+            )
+            .spacing(10)
+            .padding(20)
+            .align_items(Alignment::Center)
+            .into()
+    }
+
+    fn theme(&self) -> Self::Theme {
+        Self::Theme::Dark
+    }
+}
+
+fn main() {
+    // Load configuration file.
+    let config = load_config().unwrap();
+    // Spawn [`remote_input_client`].
+    let (input_event_tx, input_event_rx) = mpsc::channel();
+    let server_address = config.server_address.clone();
+    let api_key = config.api_key.clone();
+    let _ = thread::spawn(move || {
+        let mut remote_input_client = RemoteInputClient::new(server_address, api_key);
+        loop {
+            input_event_tx
+                .send(remote_input_client.process_event())
+                .expect("unable to send event");
+        }
+    });
+
+    // TODO: GUI
+    // TODO: Periodic and on-exit config saving
+    // TODO: Audio
+
+    if let Err(error) = Soundboard::run(Settings::default()) {
+        println!("[Main] Application error: {error}.");
+    }
+
+    /*let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let file = BufReader::new(File::open("../fart with extra reverb.mp3").unwrap());
+    let source = Decoder::new(file).unwrap();
+    stream_handle.play_raw(source.convert_samples()).unwrap();*/
 }
